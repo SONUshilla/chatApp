5cf96827a1f9e2f4f60aa0b1f1ac321661094eb7
sonu <sonushilla189@gmail.com>
2025-02-27 08:58:44 +0530

    new servers

--- src/providers/peer.jsx
+++ src/providers/peer.jsx
@@ -11,17 +11,13 @@ export const PeerProvider = ({ children }) => {
     () =>
       new RTCPeerConnection({
         iceServers: [
-          { urls: "stun:stun.l.google.com:19302" },
-          { urls: "stun:stun.l.google.com:5349" },
-          { urls: "stun:stun1.l.google.com:3478" },
-          { urls: "stun:stun1.l.google.com:5349" },
-          { urls: "stun:stun2.l.google.com:19302" },
-          { urls: "stun:stun2.l.google.com:5349" },
-          { urls: "stun:stun3.l.google.com:3478" },
-          { urls: "stun:stun3.l.google.com:5349" },
-          { urls: "stun:stun4.l.google.com:19302" },
-          { urls: "stun:stun4.l.google.com:5349" }
-        ]
+          {
+            urls: [
+              "stun:stun.l.google.com:19302",
+              "stun:global.stun.twilio.com:3478",
+            ],
+          },
+        ],
       }),
       
     []
